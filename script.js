310c52e06b6fc3883d8eb46d5e103ce4275d6c00
Nick Sedillos <nick.sedillos@gmail.com>
2018-08-07 20:28:54 -0500

    js notes

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+API key: 4a9d4a4c035e495aab2899f3e1f07991
+
+Query URL
+???
+
+Search parameters
+Search term, number of records to retrieve, start year (optional), end year (optional)
+
+Search, clear results
+
+Top articles
+
+*/
