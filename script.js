5e6589b749a8b22f00f76e0f83dad4a1577ee5bc
Kane <kmcgree14@gmail.com>
2018-08-07 21:03:06 -0500

    initial url params

--- script.js
+++ script.js
@@ -12,3 +12,16 @@ Search, clear results
 Top articles
 
 */
+
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+
+url += '?' + $.param({
+    'api-key': "4a9d4a4c035e495aab2899f3e1f07991",
+    'q': $('#search-term').val(),
+    'begin_date': $('#start-year').val(),
+    'end_date': $('#end-year').val(),
+    'page': $('#number-records').val(),
+
+});
+
+$.ajax()
